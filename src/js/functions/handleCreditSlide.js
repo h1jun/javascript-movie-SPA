3a531f6247f8469d3bd5f90a485452fb2c0ace2a
h1jun <dittoavec@gmail.com>
2021-10-03 22:00:49 +0900

    Add slide prev button

--- src/js/functions/handleCreditSlide.js
+++ src/js/functions/handleCreditSlide.js
@@ -19,39 +19,67 @@ const handleCreditSlide = () => {
         currentIdx = num;
     }
 
-    nextBtn.addEventListener('click', () => {        
+    nextBtn.addEventListener('click', () => {
         currentSlidePage++;
-    
+
         if (currentSlideCnt === slideCount) {
             // 처음으로 돌아가기
+            console.log("1");
             currentSlidePage = 1;
             currentSlideCnt = slideItems;
             moveSlide(0);
         } else if (currentSlidePage <= slidePage) {
+            console.log("2");
             moveSlide(currentIdx + slideItems);
             currentSlideCnt += slideItems;
         } else {
-            moveSlide(currentIdx + 1);
-            currentSlideCnt++;
+            // 남은 items까지만 더해주기
+            console.log("3");
+            moveSlide(currentIdx + (slideCount % slideItems));
+            currentSlideCnt += (slideCount % slideItems);
         }
     });
 
-    // prevBtn.addEventListener('click', () => {        
-    //     currentSlidePage--;
-    
-    //     if (currentSlidePage < 0) {
-    //         // 끝 페이지로 돌아가기
-    //         currentSlidePage = slidePage + 1;
-    //         currentSlideCnt = slideItems;
-    //         moveSlide(slideCount);
-    //     } else if (currentSlidePage < 0) {
-    //         moveSlide(currentIdx - slideItems);
-    //         currentSlideCnt -= slideItems;
-    //     } else {
-    //         moveSlide(currentIdx - 1);
-    //         currentSlideCnt--;
-    //     }
-    // });
+    console.log(currentSlidePage);
+    console.log(slideCount);
+    prevBtn.addEventListener('click', () => {
+        if (currentSlidePage === 1) {
+            console.log('5');
+            currentSlidePage = slidePage + 1;
+            currentSlideCnt = slideCount;
+            moveSlide(slideCount - slideItems)
+        } else if (currentSlidePage === slidePage + 1) {
+            console.log("5-1");
+            console.log(currentSlideCnt);
+            moveSlide(currentSlideCnt - (slideCount % slideItems));
+            currentSlideCnt -= (slideCount % slideItems);
+        } else if (currentSlidePage <= slidePage) {
+            console.log('6');
+            moveSlide(currentIdx - slideItems);
+            currentSlideCnt -= slideItems;
+        }
+        else if (currentSlideCnt === slideCount) {
+            console.log('4');
+            moveSlide(currentIdx - (slideCount % slideItems));
+            currentSlideCnt -= (slideCount % slideItems);
+        }
+        if (currentSlidePage !== 1) {
+            currentSlidePage--;
+        }
+
+        // if (currentSlidePage < 0) {
+        //     // 끝 페이지로 돌아가기
+        //     currentSlidePage = slidePage + 1;
+        //     currentSlideCnt = slideItems;
+        //     moveSlide(slideCount);
+        // } else if (currentSlidePage < 0) {
+        //     moveSlide(currentIdx - slideItems);
+        //     currentSlideCnt -= slideItems;
+        // } else {
+        //     moveSlide(currentIdx - 1);
+        //     currentSlideCnt--;
+        // }
+    });
 }
 
 export default handleCreditSlide;
